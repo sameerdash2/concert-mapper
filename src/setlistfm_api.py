f7a8368e3f0b8716aa1ad2345f6599f345cbf9aa
sameerdash2 <dash.sameer@gmail.com>
2024-05-21 01:55:02 -0700

    Retry setlist.fm API requests on failure

--- src/setlistfm_api.py
+++ src/setlistfm_api.py
@@ -1,6 +1,7 @@
 # setlistfm_api.py
 # Interface for the setlist.fm API. Defines several functions to interact with the API.
 
+import time
 import requests
 import json
 import os
@@ -10,6 +11,11 @@ load_dotenv()
 
 API_URL = "https://api.setlist.fm/rest/1.0/"
 API_KEY = os.getenv("SETLISTFM_API_KEY")
+
+# Max number of times to attempt an API request before giving up
+MAX_ATTEMPTS = 3
+# Delay between retries in ms
+RETRY_DELAY = 500
 
 
 def search_artist(artist_name: str) -> dict:
@@ -28,10 +34,28 @@ def search_artist(artist_name: str) -> dict:
         "Accept": "application/json"
     }
 
-    response = requests.get(url, params=params, headers=headers)
+    attempts = 0
+    success = False
+    while attempts < MAX_ATTEMPTS:
+        response = requests.get(url, params=params, headers=headers)
+        attempts += 1
 
-    if response.status_code != 200 and response.status_code != 404:
-        print(f"In search_artist('{artist_name}'): HTTP {response.status_code}: {response.text}")
+        match response.status_code:
+            # Rate limited
+            case 429:
+                print(f"Rate limited in search_artist('{artist_name}'). Waiting {RETRY_DELAY} ms.")
+            # Successful request. 404 means no results.
+            case 200 | 404:
+                success = True
+                break
+            # Unknown error. Log and retry.
+            case _:
+                print(f"In search_artist('{artist_name}'): HTTP {response.status_code}: {response.text[:100]}")
+
+        time.sleep(RETRY_DELAY / 1000)
+
+    # If request unsuccessful, raise an exception
+    if not success:
         response.raise_for_status()
 
     return json.loads(response.text)
@@ -49,10 +73,27 @@ def get_artist_setlists(artist_mbid: str) -> dict:
         "Accept": "application/json"
     }
 
-    response = requests.get(url, headers=headers)
+    attempts = 0
+    success = False
+    while attempts < MAX_ATTEMPTS:
+        response = requests.get(url, headers=headers)
+        attempts += 1
+        match response.status_code:
+            # Rate limited
+            case 429:
+                print(f"Rate limited in get_artist_setlists('{artist_mbid}'). Waiting {RETRY_DELAY} ms.")
+            # Successful request. 404 means no results.
+            case 200 | 404:
+                success = True
+                break
+            # Unknown error. Log and retry.
+            case _:
+                print(f"In get_artist_setlists('{artist_mbid}'): HTTP {response.status_code}: {response.text}")
 
-    if response.status_code != 200 and response.status_code != 404:
-        print(f"In get_artist_setlists('{artist_mbid}'): HTTP {response.status_code}: {response.text}")
+        time.sleep(RETRY_DELAY / 1000)
+    
+    # If request unsuccessful, raise an exception
+    if not success:
         response.raise_for_status()
 
     return json.loads(response.text)
