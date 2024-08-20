8e8e525a991728db20b34511f99df4f424359fdb
sameerdash2 <dash.sameer@gmail.com>
2024-08-20 16:28:05 -0700

    generalize API request code, add docs, tease the rate limit

--- server/src/setlistfm_api.py
+++ server/src/setlistfm_api.py
@@ -9,21 +9,20 @@ from dotenv import load_dotenv
 import logging
 from threading import Lock
 
-logger = logging.getLogger(__name__)
-
 load_dotenv()
 
-API_URL = "https://api.setlist.fm/rest/1.0/"
-API_KEY = os.getenv("SETLISTFM_API_KEY")
+logger = logging.getLogger(__name__)
 
-if API_KEY is None or len(API_KEY) == 0:
-    logger.warning("SETLISTFM_API_KEY has not been set")
+API_URL = "https://api.setlist.fm/rest/1.0"
+API_KEY = os.getenv("SETLISTFM_API_KEY")
 
 # Max number of times to attempt an API request before giving up
 MAX_ATTEMPTS = 3
 # Rate limit: minimum interval to aim for between requests
-RATE_LIMIT_MS = 500
-# Delay added to retries, on top of rate limit gap.
+# Official rate limit is 2 reqs per second, but they seem to support
+# a speedy turnaround for recently searched artists
+RATE_LIMIT_MS = 250
+# Delay added to retries, before request is put back into "queue".
 RETRY_DELAY = 500
 
 # Time of the last request
@@ -32,18 +31,72 @@ last_request_time = 0
 rate_limit_lock = Lock()
 
 
-def wait_for_rate_limit():
+if API_KEY is None or len(API_KEY) == 0:
+    logger.warning("SETLISTFM_API_KEY has not been set")
+
+
+def _wait_for_rate_limit() -> None:
     """Utility function that returns after a certain amount of time
     has passed since the last request."""
     global last_request_time
     with rate_limit_lock:
-        current_time = time.time()
-        # Convert to milliseconds
-        elapsed_time = (current_time - last_request_time) * 1000
+        # Store times in milliseconds (python seems to hate this)
+        current_time = time.time_ns() // 1_000_000
+        elapsed_time = current_time - last_request_time
 
         if elapsed_time < RATE_LIMIT_MS:
             time.sleep((RATE_LIMIT_MS - elapsed_time) / 1000)
-        last_request_time = time.time()
+        last_request_time = time.time_ns() // 1_000_000
+
+
+def _perform_request(path: str, params: dict, log_info: str) -> dict:
+    """Make an API request, respecting rate limits and trying multiple attempts.
+    Args:
+        path: Endpoint path, relative to base URL
+        params: Query params
+        log_info: A string to include in the log msg if an error happens
+    Returns:
+        The JSON response as a dictionary
+    Raises:
+        HTTPError: if the response code of the final attempt is not 200 or 404.
+    """
+
+    headers = {
+        "x-api-key": API_KEY,
+        "Accept": "application/json"
+    }
+    endpoint = API_URL + path
+
+    attempts = 0
+    success = False
+    while attempts < MAX_ATTEMPTS:
+        # Wait to go. This aims to respect setlist.fm rate limit, but won't stop all 429s
+        _wait_for_rate_limit()
+
+        # Request
+        response = requests.get(endpoint, params=params, headers=headers)
+        attempts += 1
+
+        match response.status_code:
+            # Successful request. 404 means no results.
+            case 200 | 404:
+                success = True
+                break
+            # Rate limited
+            case 429:
+                logger.info(f"Rate limited in {path} for '{log_info}'. Waiting {RETRY_DELAY} ms.")
+            # Unknown error.
+            case _:
+                # Sometimes they send back HTML for some reason
+                clean_response = "[HTML page]" if '<html' in response.text else response.text.rstrip()
+                logger.warning(f"Error in {path} for '{log_info}': HTTP {response.status_code}: {clean_response}")
+
+        time.sleep(RETRY_DELAY / 1000)
+
+    if not success:
+        response.raise_for_status()
+
+    return json.loads(response.text)
 
 
 def search_artist(artist_name: str) -> dict:
@@ -52,86 +105,26 @@ def search_artist(artist_name: str) -> dict:
     Raises HTTPError if the response code is not 200 or 404.
     """
 
-    url = API_URL + "search/artists"
+    path = "/search/artists"
+    # so `sort: relevance` just sorts by number of concerts,
+    # but that's the best I can do for now
     params = {
         "artistName": artist_name,
         "sort": "relevance"
     }
-    headers = {
-        "x-api-key": API_KEY,
-        "Accept": "application/json"
-    }
 
-    attempts = 0
-    success = False
-    while attempts < MAX_ATTEMPTS:
-        wait_for_rate_limit()
-        response = requests.get(url, params=params, headers=headers)
-        attempts += 1
-
-        match response.status_code:
-            # Rate limited
-            case 429:
-                logger.info(f"Rate limited in search_artist('{artist_name}'). Waiting {RETRY_DELAY} ms.")
-            # Successful request. 404 means no results.
-            case 200 | 404:
-                success = True
-                break
-            # Unknown error. Log and retry.
-            case _:
-                # Sometimes they send back HTML for some reason
-                clean_response = "[HTML page]" if '<html' in response.text else response.text.rstrip()
-                logger.warn(f"In search_artist('{artist_name}'): HTTP {response.status_code}: {clean_response}")
-
-        time.sleep(RETRY_DELAY / 1000)
-
-    # If request unsuccessful, raise an exception
-    if not success:
-        response.raise_for_status()
-
-    return json.loads(response.text)
+    return _perform_request(path, params, artist_name)
 
 
 def get_artist_setlists(artist_mbid: str, page: int) -> dict:
     """Get setlists for an artist by their MusicBrainz ID.
 
-    Raises HTTPError if the response code is not 200 or 404.
+    Raises HTTPError: if the response code is not 200 or 404.
     """
 
-    url = API_URL + "artist/" + artist_mbid + "/setlists"
+    url = f"/artist/{artist_mbid}/setlists"
     params = {
         "p": page
     }
-    headers = {
-        "x-api-key": API_KEY,
-        "Accept": "application/json"
-    }
 
-    attempts = 0
-    success = False
-    while attempts < MAX_ATTEMPTS:
-        wait_for_rate_limit()
-        response = requests.get(url, params=params, headers=headers)
-        attempts += 1
-        match response.status_code:
-            # Rate limited
-            case 429:
-                logger.info(f"Rate limited in get_artist_setlists('{artist_mbid}', {page}). Waiting {RETRY_DELAY} ms.")
-            # Successful request. 404 means no results.
-            case 200 | 404:
-                success = True
-                break
-            # Unknown error. Log and retry.
-            case _:
-                # Sometimes they send back HTML for some reason
-                clean_response = "[HTML page]" if '<html' in response.text else response.text.rstrip()
-                logger.warn(f"In get_artist_setlists('{artist_mbid}', {page}):"
-                            f" HTTP {response.status_code}: {clean_response}")
-
-        time.sleep(RETRY_DELAY / 1000)
-
-    # If request unsuccessful, raise an exception
-    if not success:
-        response.raise_for_status()
-
-    return json.loads(response.text)
+    return _perform_request(url, params, f"{artist_mbid}, page {page}")
