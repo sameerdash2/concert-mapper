294b9faae3f899ab18269080dd03420be622b8c2
sameerdash2 <dash.sameer@gmail.com>
2025-08-26 15:57:47 -0700

    Shorten about page

--- client/src/AboutView.vue
+++ client/src/AboutView.vue
@@ -3,21 +3,17 @@
   <div class="cell p-4">
     <h2>About</h2>
 
-    <p>Learn about concert mapper 📍</p>
-
     <h4>How does this website work?</h4>
     <p>
       The site looks up an artist name in the <a href="https://www.setlist.fm/">setlist.fm</a> API and then retrieves all the setlists for that artist. Setlist.fm is a large community-maintained database of songs played at concerts, known as "setlists."
     </p>
 
     <h4>Why do pins appear in a circle?</h4>
-    <p>Two things to consider:</p>
-    <p>The data returned by setlist.fm provides map coordinates for each concert, but these coordinates identify the <strong>city</strong> of the concert, not the specific venue. This means pins will appear on the geographical center of the city, rather than where the concert actually happened. (While it's probably possible to use the venue name to search for more accurate coordinates, that would mean a lot of extra requests and potential errors to handle.)</p>
-    <p>This introduces a problem: Multiple concerts in the same city will have their pins overlapped. To prevent this, I "spread" the pins into a circle around the city's center. Sometimes that process pushes pins into the ocean, but at least every pin can be clicked.</p>
+    <p>The map coordinates returned by setlist.fm identify the city of the concert, rather than the specific venue. This means multiple concerts in the same city would have their pins placed on the same spot. To prevent this, I spread the pins into a circle around the city center, so each one can be clicked.</p>
 
     <h2>Credits</h2>
     <p>Concert data is retrieved from <a href="https://www.setlist.fm/">setlist.fm</a>.</p>
-    <p>Artist images are retrieved from <a href="https://fanart.tv/">fanart.tv</a>, when available.</p>
+    <p>Artist images are retrieved from Spotify, when available.</p>
     <p>The source code is available <a href="https://github.com/sameerdash2/concert-mapper">on GitHub</a>.</p>
 
     <div class="cell has-text-right">
